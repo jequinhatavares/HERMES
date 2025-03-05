5618810362fdc984e3c7d4248140df2a1bf06bec
jequinhatavares <jequinhatavares@gmail.com>
2025-03-05 20:33:34 +0000

    comments

--- lib/Table/table.cpp
+++ lib/Table/table.cpp
@@ -4,6 +4,13 @@
 
 //bool (*isEqual)(void* a, void* b) = nullptr;
 
+/**
+ * tableCreate
+ * Creates a static table and initializes it with a comparison function.
+ *
+ * @param pFunction - A pointer to a function that compares two keys.
+ * @return TableInfo* - A pointer to the created table.
+ */
 TableInfo* tableCreate(bool (*pFunction)(void* a, void* b)){
     static TableEntry table[MaxSize];
     static TableInfo Table = {
@@ -16,6 +23,14 @@ TableInfo* tableCreate(bool (*pFunction)(void* a, void* b)){
     return &Table;
 }
 
+/**
+ * tableFind
+ * Searches for a key in the table and returns its index.
+ *
+ * @param T - A pointer to the table.
+ * @param Key - The key to search for.
+ * @return int - The index of the key if found, otherwise -1.
+ */
 int tableFind(TableInfo* T, void* Key){
     int i;
     for(i=0; i<T->numberOfItems; i++){
@@ -26,24 +41,58 @@ int tableFind(TableInfo* T, void* Key){
     return -1;
 }
 
+/**
+ * tableRead
+ * Retrieves the value associated with a given key in the table.
+ *
+ * @param T - A pointer to the table.
+ * @param key - The key to look up.
+ * @return void* - A pointer to the associated value, or nullptr if the key is not found.
+ */
 void* tableRead(TableInfo* T, void* key){
     int i = tableFind(T, key);
     if (i==-1) return nullptr;
     return T->table[i].value;
 }
 
+/**
+ * tableAdd
+ * Adds a new key-value pair to the table.
+ *
+ * @param T - A pointer to the table.
+ * @param key - The key to add.
+ * @param value - The value associated with the key.
+ * @return void
+ */
 void tableAdd(TableInfo* T, void* key, void* value){
     T->table[T->numberOfItems].key=key;
     T->table[T->numberOfItems].value=value;
     T->numberOfItems++;
 }
 
+/**
+ * tableUpdate
+ * Updates the value of an existing key in the table.
+ *
+ * @param T - A pointer to the table.
+ * @param key - The key to update.
+ * @param value - The new value to associate with the key.
+ * @return void
+ */
 void tableUpdate(TableInfo* T, void* key, void* value){
     int index = tableFind(T, key);
     if(index == -1) return;
     T->table[index].value = value;
 }
 
+/**
+ * tableRemove
+ * Removes a key-value pair from the table by shifting entries.
+ *
+ * @param T - A pointer to the table.
+ * @param key - The key to remove.
+ * @return void
+ */
 void tableRemove(TableInfo* T, void* key){
     int i, index = tableFind(T, key);
     if(index == -1) return;
