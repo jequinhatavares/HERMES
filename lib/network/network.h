f9d437f65109d78d8f1630c4a08890b425c23ec3
jequinhatavares <jequinhatavares@gmail.com>
2025-07-20 21:04:05 +0100

    User-API: organized the .h with comments

--- lib/network/network.h
+++ lib/network/network.h
@@ -9,50 +9,67 @@
 
 //#include "../lib/circular_buffer/snake_queue.h"
 
+/**
+ * @class Network
+ * @brief Integrates the node into a multi-hop, self-healing, and self-managed network.
+ *
+ * This class handles all Wi-Fi, routing, and middleware processes behind the scenes,
+ * providing a simple interface for the application to:
+ *  - Send messages across the network seamlessly,
+ *  - Perform periodic tasks,
+ *  - Choose and apply middleware strategies to influence routing behavior.
+ *
+ * It abstracts complex network logic, enabling the application to perform specific tasks
+ * without worrying about underlying networking details.
+ */
 class network {
 
     public:
 
-        // Called in setup before begin()
-        void setAsRoot(bool isRoot);
+    // ==================== NETWORK CORE =====================
+    void setAsRoot(bool isRoot);        // Set this node as root (call before begin())
+    void begin();                       // Initialize network components (call in setup())
+    void run();                         // Called in loop() to the node to run the program
 
-        void onDataReceived(void (*callback)(uint8_t*, uint8_t*, char*));
-        void onACKReceived(void (*callback)(uint8_t*, uint8_t*, char*));
-        void onPeriodicAppTask(void (*callback)());
+    // ==================== CALLBACKS ========================
+    void onDataReceived(void (*callback)(uint8_t*, uint8_t*, char*));   // Set the function that handles incoming data messages
+    void onACKReceived(void (*callback)(uint8_t*, uint8_t*, char*));    // Set the function that handles incoming ACK messages
+    void onPeriodicAppTask(void (*callback)());                         // Registers a periodic application task callback
 
-        // Called in setup()
-        void begin();
 
-        // Called in loop()
-        void run();
+    // ================== MIDDLEWARE  =================
+    void middlewareSelectStrategy(StrategyType strategyType);   // Sets the middleware routing strategy to use
+    void middlewareInfluenceRouting(char* dataMessage);         // Allows application layer to influence routing decisions (uses the currently selected middleware strategy)
 
-        /*************  Middleware related methods  ************/
-        void middlewareSelectStrategy(StrategyType strategyType);
-        void middlewareInfluenceRouting(char* dataMessage);
+    // ================ STRATEGY: INJECT =====================
+    // Configures the Inject strategy
+    void initMiddlewareStrategyInject(void *metricStruct, size_t metricStructSize,void (*setValueFunction)(void*,void*)
+                                      ,void (*encodeMetricFunction)(char*,size_t,void *),void (*decodeMetricFunction)(char*,void *));
+    void injectMetric(void*metric); // Inject new node metric into network
 
-        /*************  Strategy Inject specific methods  ************/
-        void initMiddlewareStrategyInject(void *metricStruct, size_t metricStructSize,void (*setValueFunction)(void*,void*),void (*encodeMetricFunction)(char*,size_t,void *),void (*decodeMetricFunction)(char*,void *));
-        void injectMetric(void*metric);
+    // ================ STRATEGY: PUB/SUB ====================
+    void initMiddlewareStrategyPubSub(void (*setValueFunction)(void*,void *),void (*encodeTopicFunction)(char*,size_t,void *)
+                                      ,void (*decodeTopicFunction)(char*,int8_t *)); // Configures the PubSub strategy
+    void subscribeToTopic(int8_t topic); // Subscribe to a topic
+    void unsubscribeToTopic(int8_t topic); // Remove topic subscription
+    void advertiseTopic(int8_t topic); // Advertise that this node publishes a certain topic
+    void unadvertiseTopic(int8_t topic); // Stops advertising a topic
 
-        /*************  Strategy Pub/Sub specific methods  ************/
-        void initMiddlewareStrategyPubSub(void (*setValueFunction)(void*,void *),void (*encodeTopicFunction)(char*,size_t,void *),void (*decodeTopicFunction)(char*,int8_t *));
-        void subscribeToTopic(int8_t topic);
-        void unsubscribeToTopic(int8_t topic);
-        void advertiseTopic(int8_t topic);
-        void unadvertiseTopic(int8_t topic);
 
-        /*************  Strategy Topology specific methods  ************/
-        void initMiddlewareStrategyTopology(void *topologyMetricValues, size_t topologyMetricStructSize,void (*setValueFunction)(void*,void*),void (*encodeTopologyMetricFunction)(char*,size_t,void *),void (*decodeTopologyMetricFunction)(char*,void *), uint8_t * (*selectParentFunction)(uint8_t *, uint8_t (*)[4], uint8_t));
-        void setMetric(void*metric);
+    // ================ STRATEGY: TOPOLOGY ===================
+    // Configures the Topology strategy
+    void initMiddlewareStrategyTopology(void *topologyMetricValues, size_t topologyMetricStructSize
+                                        ,void (*setValueFunction)(void*,void*),void (*encodeTopologyMetricFunction)(char*,size_t,void *)
+                                        ,void (*decodeTopologyMetricFunction)(char*,void *), uint8_t * (*selectParentFunction)(uint8_t *, uint8_t (*)[4], uint8_t));
+    void setMetric(void*metric); // Sets this node metric and sends it to the root for it to be able to discriminate between parent candidates
 
-        /*************  Message related methods  ************/
-        void sendMessageToRoot(char* messageBuffer,size_t bufferSize,const char* messagePayload);
-        void sendMessageToParent(char* messageBuffer,size_t bufferSize,const char* messagePayload);
-        void sendMessageToChildren(char* messageBuffer,size_t bufferSize,const char* messagePayload);
-        void sendMessageToNode(char* messageBuffer,size_t bufferSize,const char* messagePayload, uint8_t* nodeIP);
-        void broadcastMessage(char* messageBuffer,size_t bufferSize,const char* messagePayload);
-        void sendACKMessage(char* messageBuffer,size_t bufferSize,const char* ackPayload, uint8_t* destinationIP);
-
+    // ================== MESSAGE ROUTING ====================
+    void sendMessageToRoot(char* messageBuffer,size_t bufferSize,const char* messagePayload); // Sends a message to the root node
+    void sendMessageToParent(char* messageBuffer,size_t bufferSize,const char* messagePayload);// Sends a message to the parent node
+    void sendMessageToChildren(char* messageBuffer,size_t bufferSize,const char* messagePayload);// Sends a message to the children nodes
+    void sendMessageToNode(char* messageBuffer,size_t bufferSize,const char* messagePayload, uint8_t* nodeIP);// Sends a message to a specific node in the network
+    void broadcastMessage(char* messageBuffer,size_t bufferSize,const char* messagePayload); // Broadcasts a message to all nodes in the network
+    void sendACKMessage(char* messageBuffer,size_t bufferSize,const char* ackPayload, uint8_t* destinationIP); // Sends an ACK message to a node
 
 };
 
