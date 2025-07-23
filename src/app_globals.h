736cc21bdd2f2d3298b1be8b6a8b65f068fa15e8
jequinhatavares <jequinhatavares@gmail.com>
2025-07-23 20:36:46 +0100

    Added the enum for the DeviceType for the root node to distinguish the workers

--- src/app_globals.h
+++ src/app_globals.h
@@ -8,4 +8,22 @@ extern class Network network;  // Just a declaration
 extern char appPayload[200];
 extern char appBuffer[250];
 
+typedef enum class DeviceType{
+    DEVICE_ESP8266,
+    DEVICE_ESP_32,
+    DEVICE_RPI,
+}DeviceType;
+
+#ifdef ESP8266
+DeviceType deviceType = DeviceType::DEVICE_ESP8266;
+#endif
+
+#ifdef ESP32
+DeviceType deviceType = DeviceType::DEVICE_ESP8266;
+#endif
+
+#ifdef raspberrypi_3b
+DeviceType deviceType = DeviceType::DEVICE_ESP8266;
+#endif
+
 #endif //WIFIROUTING_APP_GLOBALS_H
