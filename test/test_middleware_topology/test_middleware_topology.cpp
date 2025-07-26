c13b99dbf49a61d1605a16ef319cfa2554b76528
jequinhatavares <jequinhatavares@gmail.com>
2025-07-26 14:44:36 +0100

    Unit Tests: Fixed strategy topology tests and removed hardcoded message types to prevent failures due to order changes

--- test/test_middleware_topology/test_middleware_topology.cpp
+++ test/test_middleware_topology/test_middleware_topology.cpp
@@ -1,9 +1,9 @@
 #include <unity.h>
 #include <cstdio>
 #include <string.h>
-#include "../lib/middleware/strategies/strategy_topology/strategy_topology.h"
-#include "table.h"
-#include "wifi_hal.h"
+#include "../lib/network/src/core/middleware/strategies/strategy_topology/strategy_topology.h"
+#include "../lib/network/src/core/table/table.h"
+#include "../lib/network/src/core/wifi_hal/wifi_hal.h"
 
 //pio test -e native -f "test_middleware_topology" -v
 
@@ -24,6 +24,8 @@ void test_encode_parent_list_advertisement_request(){
         nParents++;
     }
 
+    snprintf(correctEncodedMsg, sizeof(correctEncodedMsg),"%d %d 3.3.3.3 3.3.3.1 1.1.1.1 2.2.2.2 2.2.2.2 2.2.2.2",MIDDLEWARE_MESSAGE,TOP_PARENT_LIST_ADVERTISEMENT_REQUEST);
+
     encodeParentListAdvertisementRequest(largeSendBuffer, sizeof(largeSendBuffer), possibleParents, nParents, parentIP, mySTAIP);
 
     printf("Encoded message: %s\n", correctEncodedMsg);
@@ -37,6 +39,9 @@ void test_handle_parent_advertisement_request(){
     char PAR[100] = "10 0 1.1.1.1 1.1.1.0 2.2.2.2 5.5.5.5 5.5.5.5 5.5.5.5";
     //MESSAGE_TYPE TOP_PARENT_LIST_ADVERTISEMENT [destination IP =root] [tmpParentIP] [nodeIP] [Possible Parent 1] [Possible Parent 2] ...
     char correctEncodedMsg[100] = "10 1 4.4.4.4 1.1.1.1 2.2.2.2 5.5.5.5 5.5.5.5 5.5.5.5";
+
+    snprintf(PAR, sizeof(PAR),"%d %d 1.1.1.1 1.1.1.0 2.2.2.2 5.5.5.5 5.5.5.5 5.5.5.5",MIDDLEWARE_MESSAGE,TOP_PARENT_LIST_ADVERTISEMENT_REQUEST);
+    snprintf(correctEncodedMsg, sizeof(correctEncodedMsg),"%d %d 4.4.4.4 1.1.1.1 2.2.2.2 5.5.5.5 5.5.5.5 5.5.5.5",MIDDLEWARE_MESSAGE,TOP_PARENT_LIST_ADVERTISEMENT);
 
     handleMessageStrategyTopology(PAR, sizeof(PAR));
 
@@ -68,11 +73,15 @@ void test_root_handle_message_metrics_report(){
 
     initStrategyTopology(topologyMetrics, sizeof(topologyTableEntry),setTopologyMetricValue,encodeTopologyMetricEntry,decodeTopologyMetricEntry,chooseParentByProcessingCapacity);
 
+    snprintf(MR1, sizeof(MR1),"%d %d 1.1.1.1 2.2.2.2 2",MIDDLEWARE_MESSAGE,TOP_METRICS_REPORT);
+    snprintf(MR2, sizeof(MR2),"%d %d 1.1.1.1 3.3.3.3 3",MIDDLEWARE_MESSAGE,TOP_METRICS_REPORT);
+    snprintf(MR3, sizeof(MR3),"%d %d 1.1.1.1 4.4.4.4 4",MIDDLEWARE_MESSAGE,TOP_METRICS_REPORT);
+
     handleMessageStrategyTopology(MR1, sizeof(MR1));
     handleMessageStrategyTopology(MR2, sizeof(MR2));
     handleMessageStrategyTopology(MR3, sizeof(MR3));
 
-    //tablePrint(topologyMetricsTable,printTopologyMetricStruct);
+    //tablePrint(topologyMetricsTable,printTopologyTableHeader,printTopologyMetricStruct);
 
     topologyTableEntry *entry1= (topologyTableEntry*) tableRead(topologyMetricsTable,IP1);
     TEST_ASSERT(entry1 != nullptr);
@@ -86,7 +95,7 @@ void test_root_handle_message_metrics_report(){
     TEST_ASSERT(entry3 != nullptr);
     TEST_ASSERT(entry3->processingCapacity == 4);
 
-    tableClean(topologyMetricsTable);
+    tableClean(topologyMetricsTable);/******/
 
 }
 
@@ -100,14 +109,23 @@ void test_root_handle_parent_advertisement_request(){
     iamRoot = true;
     initStrategyTopology(topologyMetrics, sizeof(topologyTableEntry),setTopologyMetricValue,encodeTopologyMetricEntry,decodeTopologyMetricEntry,chooseParentByProcessingCapacity);
 
+    snprintf(MR1, sizeof(MR1),"%d %d 1.1.1.1 2.2.2.2 2",MIDDLEWARE_MESSAGE,TOP_METRICS_REPORT);
+    snprintf(MR2, sizeof(MR2),"%d %d 1.1.1.1 3.3.3.3 3",MIDDLEWARE_MESSAGE,TOP_METRICS_REPORT);
+    snprintf(MR3, sizeof(MR3),"%d %d 1.1.1.1 4.4.4.4 4",MIDDLEWARE_MESSAGE,TOP_METRICS_REPORT);
+
+
     handleMessageStrategyTopology(MR1, sizeof(MR1));
     handleMessageStrategyTopology(MR2, sizeof(MR2));
     handleMessageStrategyTopology(MR3, sizeof(MR3));
 
+    snprintf(PAR, sizeof(PAR),"%d %d 1.1.1.1 1.1.1.0 5.5.5.5 2.2.2.2 3.3.3.3",MIDDLEWARE_MESSAGE,TOP_PARENT_LIST_ADVERTISEMENT_REQUEST);
+
     handleMessageStrategyTopology(PAR, sizeof(PAR));
 
-    //printf("Encoded message: %s\n", correctEncodedMsg);
-    //printf("Encoded message: %s\n", smallSendBuffer);
+    snprintf(correctEncodedMsg, sizeof(correctEncodedMsg),"%d %d 5.5.5.5 5.5.5.5 3.3.3.3",MIDDLEWARE_MESSAGE,TOP_PARENT_ASSIGNMENT_COMMAND);
+
+    printf("Correct message: %s\n", correctEncodedMsg);
+    printf("Encoded message: %s\n", smallSendBuffer);
 
     TEST_ASSERT(strcmp(smallSendBuffer,correctEncodedMsg) == 0);
 
@@ -119,7 +137,7 @@ void test_root_handle_parent_list_advertisement(){
     //MESSAGE_TYPE TOP_METRICS_REPORT [destination:rootIP] [nodeIP] [metric]
     char MR1[100] = "10 3 1.1.1.1 2.2.2.2 2",MR2[100] = "10 3 1.1.1.1 3.3.3.3 3",MR3[100] = "10 3 1.1.1.1 4.4.4.4 4";
     char PLA[100] = "10 1 1.1.1.1 2.2.2.2 5.5.5.5 2.2.2.2 4.4.4.4";
-    char correctEncodedMsg[100] = "10 2 2.2.2.2 5.5.5.5 4.4.4.4";
+    char correctEncodedMsg[100];
     uint8_t IP1[4]={2,2,2,2}, IP2[4]={3,3,3,3}, IP3[4]={4,4,4,4};
 
     uint8_t root[4]={1,1,1,1};
@@ -129,11 +147,19 @@ void test_root_handle_parent_list_advertisement(){
 
     initStrategyTopology(topologyMetrics, sizeof(topologyTableEntry),setTopologyMetricValue,encodeTopologyMetricEntry,decodeTopologyMetricEntry,chooseParentByProcessingCapacity);
 
+    snprintf(MR1, sizeof(MR1),"%d %d 1.1.1.1 2.2.2.2 2",MIDDLEWARE_MESSAGE,TOP_METRICS_REPORT);
+    snprintf(MR2, sizeof(MR2),"%d %d 1.1.1.1 3.3.3.3 3",MIDDLEWARE_MESSAGE,TOP_METRICS_REPORT);
+    snprintf(MR3, sizeof(MR3),"%d %d 1.1.1.1 4.4.4.4 4",MIDDLEWARE_MESSAGE,TOP_METRICS_REPORT);
+
     handleMessageStrategyTopology(MR1, sizeof(MR1));
     handleMessageStrategyTopology(MR2, sizeof(MR2));
     handleMessageStrategyTopology(MR3, sizeof(MR3));
 
+    snprintf(PLA, sizeof(PLA),"%d %d 1.1.1.1 2.2.2.2 5.5.5.5 2.2.2.2 4.4.4.4",MIDDLEWARE_MESSAGE,TOP_PARENT_LIST_ADVERTISEMENT);
+
     handleMessageStrategyTopology(PLA, sizeof(PLA));
+
+    snprintf(correctEncodedMsg, sizeof(correctEncodedMsg),"%d %d 2.2.2.2 5.5.5.5 4.4.4.4",MIDDLEWARE_MESSAGE,TOP_PARENT_ASSIGNMENT_COMMAND);
 
     //printf("Encoded message: %s\n", correctEncodedMsg);
     //printf("Encoded message: %s\n", smallSendBuffer);
@@ -154,6 +180,8 @@ void test_root_handle_parent_advertisement_without_any_metric(){
     iamRoot = true;
     initStrategyTopology(topologyMetrics, sizeof(topologyTableEntry),setTopologyMetricValue,encodeTopologyMetricEntry,decodeTopologyMetricEntry,chooseParentByProcessingCapacity);
 
+    snprintf(PLA, sizeof(PLA),"%d %d 1.1.1.1 2.2.2.2 5.5.5.5 2.2.2.2 4.4.4.4",MIDDLEWARE_MESSAGE,TOP_PARENT_LIST_ADVERTISEMENT);
+
     handleMessageStrategyTopology(PLA, sizeof(PLA));
 
     //printf("Encoded message: %s\n", correctEncodedMsg);
@@ -170,8 +198,9 @@ void test_other_node_handle_parent_list_advertisement(){
     char MR1[100] = "10 3 1.1.1.1 2.2.2.2 2",MR2[100] = "10 3 1.1.1.1 3.3.3.3 3",MR3[100] = "10 3 1.1.1.1 4.4.4.4 4";
     char PLA[100] = "10 1 4.4.4.4 2.2.2.2 5.5.5.5 2.2.2.2 4.4.4.4";
     char correctEncodedMsg[100] = "10 2 2.2.2.2 5.5.5.5 4.4.4.4";
-    uint8_t IP1[4]={2,2,2,2}, IP2[4]={3,3,3,3}, IP3[4]={4,4,4,4};
 
+
+    snprintf(PLA, sizeof(PLA),"%d %d 4.4.4.4 2.2.2.2 5.5.5.5 2.2.2.2 4.4.4.4",MIDDLEWARE_MESSAGE,TOP_PARENT_LIST_ADVERTISEMENT);
 
     handleMessageStrategyTopology(PLA, sizeof(PLA));
 
@@ -210,8 +239,8 @@ int main(int argc, char** argv){
     RUN_TEST(test_init_strategy_topology);
     RUN_TEST(test_root_handle_message_metrics_report);
     RUN_TEST(test_root_handle_parent_advertisement_request);
-    RUN_TEST(test_root_handle_parent_list_advertisement);
+    RUN_TEST(test_root_handle_parent_list_advertisement);/******/
     RUN_TEST(test_root_handle_parent_advertisement_without_any_metric);
-    RUN_TEST(test_other_node_handle_parent_list_advertisement);
+    RUN_TEST(test_other_node_handle_parent_list_advertisement);/******/
     UNITY_END();
 }
